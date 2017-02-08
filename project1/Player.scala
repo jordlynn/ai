5037a48e3dc49bd7622128b365d9caab2abf35de
jordlynn <snake.river52@gmail.com>
2017-02-08 15:38:02 -0800

    valid moves presented as tiles now

--- project1/Player.scala
+++ project1/Player.scala
@@ -6,17 +6,21 @@ class Player (algorithmType: String) {
     var closedList: List[Int] = List() // start empty
 
     def BreadthFirstSearch() = {
-        closedList = List()
         var zeroTuple = FindZero(Puzzel.TileBoard)
         var zeroNumberedPosition = ConvertCord(zeroTuple(0)._1, zeroTuple(0)._2)
-        var neighbors = validneighbors(zeroNumberedPosition)
-        closedList = closedList ++ neighbors
-        println("List " + closedList)
+        var neighbors = validneighbors(zeroTuple(0)._1, zeroTuple(0)._2)
+        closedList = neighbors.map(x => ConvertCord(x(0), x(1)))
+        closedList = closedList.filter(_ > 0)
+        
     }
 
     /* Note these aren't sanity checked! */
-    private def validneighbors(zeroPosition: Int): List[Int] = 
-        List(zeroPosition + 1, zeroPosition - 1, zeroPosition + 3, zeroPosition - 3 )
+    private def validneighbors(xCord: Int, yCord: Int): List[List[Int]] = 
+        List(
+            List(xCord - 1, yCord), List(xCord + 1, yCord),
+            List(xCord, yCord - 1), List(xCord, yCord + 1)
+        )
+    
 
     private def FindZero(tmpTileBoard: Array[Array[Int]]) = 
         for {
@@ -32,12 +36,15 @@ class Player (algorithmType: String) {
             case 0 => ytmpList = List(1, 4, 7)
             case 1 => ytmpList = List(2, 5, 8)
             case 2 => ytmpList = List(3, 6, 9)
+            case _ => return 0
         }
         xCord match {
             case 0 => xtmpList = List(1, 2, 3)
             case 1 => xtmpList = List(4, 5, 6)
             case 2 => xtmpList = List(7, 8, 9)
+            case _ => return 0
         }
+
         (xtmpList intersect ytmpList)(0) // Return converted coordinate
         
     }
